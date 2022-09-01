8418fa472af830a6bdb2e286d93d867c2f3a272f
Suhan Park <psh117@snu.ac.kr>
2022-09-01 14:28:38 +0900

    update for multi cameras

--- receiver.py
+++ receiver.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python
 
-from __future__ import division
 import cv2
 import numpy as np
 import socket
 import struct
+import time
+from multiprocessing import Process
 
+ADDR = '192.168.1.40'
 MAX_DGRAM = 2**16
 
 def dump_buffer(s):
     """ Emptying buffer frame """
     while True:
         seg, addr = s.recvfrom(MAX_DGRAM)
-        print(seg[0])
+        # print(seg[0])
         if struct.unpack("B", seg[0:1])[0] == 1:
-            print("finish emptying buffer")
+            # print("finish emptying buffer")
             break
 
-def main():
-    """ Getting image udp frame &
-    concate before decode and output image """
-    
-    # Set up socket
+def run_viewer(video_id):
+    port = 41107 + video_id
+    print (f'[{video_id}] open port with {port}')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('127.0.0.1', 12345))
+    s.bind((ADDR, port))
+    print (f'[{video_id}] success')
     dat = b''
     dump_buffer(s)
+    befo_time = time.time()
+    dts = np.zeros(60)
+    idx = 0
 
     while True:
+        dt = time.time() - befo_time
+        dts[idx] = dt
+        idx += 1
+
+        if idx == 60:
+            idx = 0
+            print(f'[{video_id}] frame rate:', 1/dts.mean())
+
+        befo_time = time.time()
+
         seg, addr = s.recvfrom(MAX_DGRAM)
         if struct.unpack("B", seg[0:1])[0] > 1:
             dat += seg[1:]
@@ -38,10 +52,20 @@ def main():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             dat = b''
+            # dump_buffer(s)
 
     # cap.release()
     cv2.destroyAllWindows()
     s.close()
+
+
+def main():
+    p1 = Process(target=run_viewer, args=(0,))
+    p2 = Process(target=run_viewer, args=(2,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
 if __name__ == "__main__":
     main()
--- sender.py
+++ sender.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-from __future__ import division
 import cv2
 import numpy as np
 import socket
 import struct
-import math
+import math, time
+from multiprocessing import Process
+
+# ADDR = '192.168.1.51'
+ADDR = '192.168.1.40'
+WIDTH = 1280
+HEIGHT = 720
+FPS = 120
+QUALITY = 30
 
 
 class FrameSegment(object):
@@ -15,18 +22,20 @@ class FrameSegment(object):
     """
     MAX_DGRAM = 2**16
     MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # extract 64 bytes in case UDP frame overflown
-    def __init__(self, sock, port, addr="127.0.0.1"):
+    def __init__(self, sock, port, addr=ADDR):
         self.s = sock
         self.port = port
         self.addr = addr
 
-    def udp_frame(self, img):
+    def udp_frame(self, img1):
         """ 
         Compress image and Break down
         into data segments 
         """
-        compress_img = cv2.imencode('.jpg', img)[1]
-        dat = compress_img.tostring()
+        compress_img1 = cv2.imencode('.jpg', img1, [int(cv2.IMWRITE_JPEG_QUALITY), QUALITY])[1]
+
+        # fdsa
+        dat = compress_img1.tostring() 
         size = len(dat)
         count = math.ceil(size/(self.MAX_IMAGE_DGRAM))
         array_pos_start = 0
@@ -40,21 +49,46 @@ class FrameSegment(object):
             count -= 1
 
 
-def main():
-    """ Top level main function """
-    # Set up UDP socket
+def run_sending(video_id = 0):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    port = 12345
-
+    port = 41107 + video_id
+    print (f'[{video_id}] open port with {port}')
     fs = FrameSegment(s, port)
 
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(video_id, cv2.CAP_ANY, 
+                            [cv2.CAP_PROP_FRAME_WIDTH, 
+                             WIDTH, 
+                             cv2.CAP_PROP_FRAME_HEIGHT, 
+                             HEIGHT, 
+                             cv2.CAP_PROP_FPS,
+                             FPS])
+    befo_time = time.time()
+    dts = np.zeros(60)
+    idx = 0
     while (cap.isOpened()):
+        dt = time.time() - befo_time
+        dts[idx] = dt
+        idx += 1
+
+        if idx == 60:
+            idx = 0
+            print(f'[{video_id}] frame rate:', 1/dts.mean())
+
+        befo_time = time.time()
         _, frame = cap.read()
         fs.udp_frame(frame)
     cap.release()
     cv2.destroyAllWindows()
     s.close()
+    pass
+
+def main():
+    p1 = Process(target=run_sending, args=(0,))
+    p2 = Process(target=run_sending, args=(2,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
 if __name__ == "__main__":
     main()
